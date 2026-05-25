66f8dd3a574404deb9629362fb7a7f49654ca631
Francisco <franciscopaulino100@outlook.com>
2026-05-25 20:07:50 -0300

    gitkeep

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+// Importe a biblioteca foundation do Flutter para checar a plataforma
+import 'package:flutter/foundation.dart'; 
 
 void main() {
-  runApp(const MainApp());
+  // Cria uma variável que verifica se está rodando no Linux, Windows ou Mac
+  bool isDesktop = defaultTargetPlatform == TargetPlatform.linux || 
+                   defaultTargetPlatform == TargetPlatform.windows ||
+                   defaultTargetPlatform == TargetPlatform.macOS;
+
+  runApp(
+    DevicePreview(
+      // O preview só será ativado nos computadores de mesa.
+      // Nos emuladores (TargetPlatform.android), ele desativa sozinho!
+      enabled: isDesktop, 
+      builder: (context) => const MeuApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      title: 'App da Equipe',
+      home: const Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Olá Equipe!'),
         ),
       ),
     );
